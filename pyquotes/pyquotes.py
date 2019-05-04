873d8e4f7748fc248deea9af9ad169fcf237c1e0
akshatkarani <akshatkarani@gmail.com>
2019-05-04 14:37:19 +0530

    UPdates

--- pyquotes/pyquotes.py
+++ pyquotes/pyquotes.py
@@ -3,33 +3,36 @@
 # 1. Get the quote based on certain category and person
 # 2. Get the quote of the day
 
-def get_quotes(person: (None, str) = None,
-			   category: (None, str) = None):
-	"""
-	This function returns all the quotes that matches the input.
 
-	:param person:	 Name of the person e.g. Albert Einstein
-	:param category: Category of quote e.g. Motivational
-	:param return:   List of tuples [(quote, author_of_the_quote), ..]
-	"""
-	pass
+def get_quotes(person: (None, str) = None,
+               category: (None, str) = None):
+    """
+    This function returns all the quotes that matches the input.
+
+    :param person:	 Name of the person e.g. Albert Einstein
+    :param category: Category of quote e.g. Motivational
+    :param return:   List of tuples [(quote, author_of_the_quote), ..]
+    """
+    pass
+
 
 def get_quote(person: (None, str) = None,
-			  category: (None, str) = None):
-	"""
-	This function take a category and a person as a input and returns
-	a random quote which matches the input.
+              category: (None, str) = None):
+    """
+    This function take a category and a person as a input and returns
+    a random quote which matches the input.
 
-	:param person:	 Name of the person e.g. Albert Einstein
-	:param category: Category of quote e.g. Motivational
-	:param return:   A tuple (quote, author_of_the_quote)
-	"""
-	pass
+    :param person:	 Name of the person e.g. Albert Einstein
+    :param category: Category of quote e.g. Motivational
+    :param return:   A tuple (quote, author_of_the_quote)
+    """
+    pass
+
 
 def get_quote_of_the_day():
-	"""
-	This fuction returns quote of the day.
+    """
+    This fuction returns quote of the day.
 
-	:param return: A tuple (quote, author_of_the_quote)
-	"""
-	pass
+    :param return: A tuple (quote, author_of_the_quote)
+    """
+    pass
