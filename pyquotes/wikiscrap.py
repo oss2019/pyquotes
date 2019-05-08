4682597291a22a6017e70125035f5b8b2baa33d3
rka87338 <rka87338@gmail.com>
2019-05-08 11:18:43 +0530

    changed things according to pep8

--- pyquotes/wikiscrap.py
+++ pyquotes/wikiscrap.py
@@ -2,9 +2,11 @@ from bs4 import BeautifulSoup
 import requests
 
 source = requests.get("https://en.wikiquote.org/wiki/Main_Page").text
-soup  = BeautifulSoup(source,"lxml")
-#names is  list of all names with links in the main page of wikiquote
-names = soup.find('div',class_='mw-parser-output').find_all('div')[11].find_all('p')[1].find_all('a')
+soup = BeautifulSoup(source, "lxml")
+# names is  list of all names with links in the main page of wikiquote
+names_path = soup.find('div', class_='mw-parser-output').find_all('div')[11]
+names = names_path.find_all('p')[1].find_all('a')
+
 
 def get_quotes(person):
     quotes_by_author = list()
@@ -12,18 +14,18 @@ def get_quotes(person):
         if (person == name.text.lower()):
             link = "https://en.wikiquote.org" + name['href']
             link = requests.get(link).text
-            soup_for_indiv = BeautifulSoup(link,"lxml")
-            quotes = soup_for_indiv.find_all('div',class_='mw-parser-output')[0].find_all('ul')
+            soup_for_indiv = BeautifulSoup(link, "lxml")
+            quotes = soup_for_indiv.find_all('div', class_='mw-parser-output')[0].find_all('ul')
             for quote in quotes:
-                try:
-                    if (quote.li.b==None):                  #if there is no quote
+                try: 
+                    if quote.li.b is None:                   
                         continue
-                    elif quote.li.b.text.isdigit()==True:   #so that there aren't any numbers
+                    elif quote.li.b.text.isdigit():    
                         continue
-                    elif len(quote.li.b.text.split(' '))<2: #so that there aren't any words
+                    elif len(quote.li.b.text.split(' ')) < 2:  
                         continue    
                     else:
-                        temp = [quote.li.b.text,name.text]
+                        temp = [quote.li.b.text, name.text]
                         quotes_by_author.append(tuple(temp))
                 except:
                     continue
