d79bef27894bb37ac92b2c5fce55e417d411b583
Sayan Mondal <mondalsayan556@gmail.com>
2019-07-04 01:11:30 +0530

    Code Formator and Basic brainyquotes test added

---
+++ pyquotes/__init__.py
@@ -0,0 +1,4 @@
+__all__ = ["pyquotes",
+             "brainyquote",
+             "scrapper",
+              ]
---
+++ tests/test_brainyquotes.py
@@ -0,0 +1,13 @@
+
+import requests
+
+class TestBrainyQuotes:
+    def TestBrainyQuotes():
+        
+        r = requests.head("https://www.brainyquote.com/")
+        print("Website Up")
+        return (r.status_code == 200)
+
+if __name__ == "__main__":
+    TestBrainyQuotes.TestBrainyQuotes()
+        
