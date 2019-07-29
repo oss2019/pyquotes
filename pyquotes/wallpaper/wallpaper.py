9c1948dc871020a7674d1d6c9b1cd88f0bd78fa4
Harsh Raj <harshraj22aug@gmail.com>
2019-07-29 13:12:39 +0530

    updated as per pep8

--- pyquotes/wallpaper/wallpaper.py
+++ pyquotes/wallpaper/wallpaper.py
@@ -4,29 +4,31 @@ import random
 import os
 
 # website to get wallpapers
-parent_link = 'https://pixabay.com/images/search/wallpaper/'
+parent_link = "https://pixabay.com/images/search/wallpaper/"
+
 
 def get_wallpapers():
-	""" returns a list of links of wallpapers """
-	lst = list()
-	item = requests.get(parent_link).text
-	soup = BeautifulSoup(item,'lxml')
-	var = soup.find_all('div',attrs = {'class':'item'})
-	for images in var:
-		image = images.find('img')
-		interest = image.get('src')
-		if 'http' in interest:
-			lst.append(interest)
-	return lst
+    """ returns a list of links of wallpapers """
+    lst = list()
+    item = requests.get(parent_link).text
+    soup = BeautifulSoup(item, "lxml")
+    var = soup.find_all("div", attrs={"class": "item"})
+    for images in var:
+        image = images.find("img")
+        interest = image.get("src")
+        if "http" in interest:
+            lst.append(interest)
+    return lst
 
-if __name__=='__main__':
-	lst = get_wallpapers()
-	image = random.choice(lst)
-	name = image[47:]
-	r = requests.get(image)
-	with open(name,'wb') as f:
-		f.write(r.content)
-	print('downloaded ',name)
+
+if __name__ == "__main__":
+    lst = get_wallpapers()
+    image = random.choice(lst)
+    name = image[47:]
+    r = requests.get(image)
+    with open(name, "wb") as f:
+        f.write(r.content)
+    print("downloaded ", name)
 
 
 # use the following to automate using cron tab every midnight
